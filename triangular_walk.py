1168b6ffcbd3bbf13a6c8fbcbefd9440b76d9994
Jasper Lee <jasperchlee@brown.edu>
2018-10-08 00:40:44 -0400

    Default coefficients for nmax = 15
    The coefficients may be a bit too delegate and really pushing the
    independence assumption. We should see if these coefficients work, or
    we need a deeper nmax and smoother coefficients.

--- triangular_walk.py
+++ triangular_walk.py
@@ -1,19 +1,22 @@
 import numpy as np
 import math
 
-def check_triangle_completion(n_, k_, n_max=50):
+default_coeff = {
+    8: 0,
+    9: 6.91310814435742,
+    10: 5.03184709494337,
+    11: 2.10144000103314,
+    12: 0.635599038344795,
+    13: 1.96455080814112,
+    14: 1.01560222816783,
+    15: 1.00912038379987,
+}
 
-    estimate_ = 0.
-    if n_ < n_max and k_/n_ > 0.5:
-        return False, estimate_
+def check_triangle_completion(n_, k_, n_max=15, coefficients=default_coeff):
+    if n_ < n_max and k_*1./n_ > 0.5:
+        return False, 0.
     else:
-        if k_/n_ <= 0.5:
-            return True, estimate_
-        if (2-n_max-2*k_)**2 -4*(2*n_max-2)*k_ >= 0:
-            p_ = ( 2.*k_+n_max-2+math.sqrt((2-n_max-2*k_)**2-4*(2*n_max-2)*k_)) / (4.*n_max-4)
+        if k_*1./n_ <= 0.5:
+            return True, 0.
         else:
-            p_ = ((2.*k_+n_max-2)/(4*n_max-4))
-        p_ = max(p_, 0.6)
-        estimate_ = 1./(2*p_-1.) - 0.165*p_*(1-p_)/(2*p_ - 1)**2
-
-        return True, estimate_
+            return True, coefficients[k_]
